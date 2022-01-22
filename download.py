849062e5d62a754d2b98fced4e31584040b9450d
Francis Dela Cruz <50617340+fdc981@users.noreply.github.com>
2022-01-22 21:37:45 +1030

    Change data processing pipeline
    The previous method of processing the FCAS price data first downloaded the daily dispatch reports via a script, which was then processed on import of `src.data`. A DataFrame of a single day of SA FCAS prices could then be accessed by a module attribute `sa_price_df`. However, this was inconvenient for when data from other days were needed, as the procedure for data extraction was hard-coded.
    
    This commit fixes this by replacing the code of data.py to have only three functions for the data processing and retrieval of SA FCAS prices:
    
    - download_daily_reports (code from download.py)
    - extract_sa_fcas_prices (code from old data.py initialisation)
    - get_sa_fcas_prices
    
    download.py has also been edited accordingly to output a csv of SA FCAS prices under data/.

--- download.py
+++ download.py
@@ -1,33 +1,6 @@
-import requests
-import pathlib
-import bs4
-import re
-import shutil
-import os
+from src.data import download_daily_reports, extract_sa_fcas_prices
 
-response = requests.get("https://www.nemweb.com.au/REPORTS/CURRENT/Daily_Reports/")
-soup = bs4.BeautifulSoup(response.text)
-zip_links = soup.find_all('a', string=re.compile('.zip'))
+download_daily_reports()
 
-# path string of the output folder
-out_path = "data/"
-
-# Download all zip files
-pathlib.Path(out_path).mkdir(parents=True, exist_ok=True)
-
-for a_tag in zip_links:
-    filename = a_tag.attrs['href'].split('/')[-1]
-
-    if not pathlib.Path("data/" + filename.replace("zip", "CSV")).exists():
-        url = "https://www.nemweb.com.au" + a_tag.attrs['href']
-        response = requests.get(url)
-
-        with open(f"{out_path}/{filename}", 'wb') as f:
-            f.write(response.content)
-
-# Extract and remove each zip file
-zip_paths = pathlib.Path(out_path).glob('*.zip')
-
-for path in zip_paths:
-    shutil.unpack_archive(str(path), str(path.parent))
-    os.remove(str(path))
+df = extract_sa_fcas_prices()
+df.to_csv("data/sa_fcas_prices.csv")
