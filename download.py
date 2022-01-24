42bfc70f8ff0e279f0484374eee7aa326ececa9b
Francis Dela Cruz <50617340+fdc981@users.noreply.github.com>
2022-01-24 14:34:46 +1030

    Reset index upon compiling CSV
    Upon extraction of the data, the resulting index (which was a result of appending multiple different rows from different DataFrames) was not being removed. This commit fixes this.

--- download.py
+++ download.py
@@ -3,4 +3,5 @@ from src.data import download_daily_reports, extract_sa_fcas_prices
 download_daily_reports()
 
 df = extract_sa_fcas_prices()
+df.reset_index(drop=True, inplace=True)
 df.to_csv("data/sa_fcas_prices.csv")
