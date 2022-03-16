04690e62f761433579aac4954e289f8fe96b66b4
Francis Dela Cruz <50617340+fdc981@users.noreply.github.com>
2022-03-16 16:19:21 +1030

    Include price scenarios in make_scenario_model
    Add price scenarios to make_scenario_model. Price scenarios are combined with enablement scenarios (via pointwise multiplication) to create a scenario. This commit adds the parameter `scenario_combine_method` to indicate whether to combine all combinations of price and enablement (with 'product') or to combine corresponding price and enablement scenarios in order of their appearance (with 'zip').

--- src/models.py
+++ src/models.py
@@ -219,6 +219,7 @@ def make_scenario_model(
         prices='auto',
         enablement_probabilities=None,
         enablement_scenarios=None,
+        scenario_combine_method='product',
         efficiency_in=0.92,
         efficiency_out=0.90,
         p_min=0,
@@ -237,11 +238,13 @@ def make_scenario_model(
         M: value of big M for binary indicator constraints
         epsilon: an arbitrarily small value
         initial_soc: initial state of charge (in MWh)
-        prices: a dictionary with an array of prices associated with each of
-            the contingency FCAS services. The keys for each service should be
-            as follows: `"lower_6_sec", "raise_6_sec", "lower_60_sec",
-            "raise_60_sec", "lower_5_min", "raise_5_min"`. Each array should
-            be of size `n`.
+        prices: a dictionary with an array or matrix of prices associated with
+            each of the contingency FCAS services. The keys for each service
+            should be as follows: `"lower_6_sec", "raise_6_sec",
+            "lower_60_sec", "raise_60_sec", "lower_5_min", "raise_5_min"`.
+            When values are arrays, then they must be of length `n`. Otherwise
+            the values are matrices, which should be of shape
+            `(num_scenarios, n)`.
         enablement_probabilities: a dictionary with a matrix of enablement
             probabilities associated with each of the contingency FCAS.
         enablement_scenarios: a dictionary with a matrix of enablement
@@ -249,6 +252,7 @@ def make_scenario_model(
             The keys for each service should be as follows: `"lower_6_sec",
             "raise_6_sec", "lower_60_sec", "raise_60_sec", "lower_5_min",
             "raise_5_min"`. Each array should be of size `n`.
+        scenario_combine_method: either 'product' or 'zip'.
         efficiency_in: the charging efficiency, as a proportion
         efficiency_out: the discharging efficiency, as a proportion
         p_min: minimum charge/discharge power limit (in MW)
@@ -256,8 +260,7 @@ def make_scenario_model(
         soc_min: the minimum amount of stored energy (in MWh)
         soc_max: the maximum amount of stored energy (in MWh)
         prices_from: if `prices == 'auto'`, then this is the start date to
-            retrieve prices from `data/sa_fcas_data.csv`. If None, retrieves
-            the first `n` prices from the price CSV.
+            retrieve arrays of prices from `data/sa_fcas_data.csv`.
         log: allow Gurobi to output during optimization.
     """
 
@@ -274,7 +277,6 @@ def make_scenario_model(
         raise Exception("Please provide a valid value for enablement_scenarios.")
 
     T = [i for i in range(n)]
-    S = [i for i in range(num_scenarios)]
 
     F_lower = ["lower_6_sec", "lower_60_sec", "lower_5_min"]
     F_raise = ["raise_6_sec", "raise_60_sec", "raise_5_min"]
@@ -304,9 +306,31 @@ def make_scenario_model(
     soc = m.addVars(T, vtype='C', name='soc', lb=soc_min, ub=soc_max)
     assert soc_min <= initial_soc and initial_soc <= soc_max
 
-    scenario_weights = enablement_scenario_weights(num_scenarios, enablement_scenarios, enablement_probabilities)
+    scenarios = {}
+    en_scenario_weights = enablement_scenario_weights(num_scenarios, enablement_scenarios, enablement_probabilities)
+    scenario_weights = {f: [] for f in F}
 
-    m.setObjective(sum((scenario_weights[s] * enablement_scenarios[f][s, t] * prices[f][t] * p[f, t] for f, t, s in product(F, T, S))) / 12, gp.GRB.MAXIMIZE)
+    if scenario_combine_method == 'product':
+        for f in F:
+            scenarios[f] = []
+            for s1, s2 in product(range(num_scenarios), repeat=2):
+                scenarios[f].append(prices[f][s1, :] * enablement_scenarios[f][s2, :])
+                scenario_weights[f].append(en_scenario_weights[s2])
+            scenarios[f] = np.array(scenarios[f])
+    elif scenario_combine_method == 'zip':
+        assert all((prices[f].shape == enablement_scenarios[f].shape for f in F))
+        for f in F:
+            scenarios[f] = prices[f] * enablement_scenarios[f]
+            scenario_weights[f].append(scenario_weights)
+    else:
+        raise Exception("Note: scenarios not done")
+
+    S = [i for i in range(len(scenarios[F[0]]))]
+
+
+    print("scenarios shapes:", [scenarios[f].shape for f in F])
+
+    m.setObjective(sum((scenario_weights[f][s] * scenarios[f][s, t] * p[f, t] for f, t, s in product(F, T, S))) / 12, gp.GRB.MAXIMIZE)
 
     m.addConstr(soc[0] == initial_soc + sum((p[f, 0] * delta_t[f] for f in F_lower))
                                       - sum((p[f, 0] * delta_t[f] for f in F_raise)))
