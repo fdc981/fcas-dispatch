d06cdf9a9a89dca23ab59298c56383829a14a4cd
Francis Dela Cruz <50617340+fdc981@users.noreply.github.com>
2022-04-19 20:51:46 +0930

    Add module containing constants used throughout the codebase

--- src/constants.py
+++ src/constants.py
@@ -1,5 +1,7 @@
 """Common constants used in the optimisation models and helper functions."""
 
+services = ["RAISE5MIN", "LOWER5MIN", "RAISE60SEC", "LOWER60SEC", "RAISE6SEC", "LOWER6SEC"]
+
 F_lower = ["lower_6_sec", "lower_60_sec", "lower_5_min"]
 F_raise = ["raise_6_sec", "raise_60_sec", "raise_5_min"]
 F = F_lower + F_raise
