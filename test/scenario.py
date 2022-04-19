eea22bb8f56d5ec26ca6a633075378ffce3ccab6
Francis Dela Cruz <50617340+fdc981@users.noreply.github.com>
2022-04-19 21:47:22 +0930

    Add unit tests for scenario generation algorithm
    Add a unit test for the functions that constitute the scenario generation algorithm in src/scenario.py. The unit tests currently check two properties to verify correctness, namely that the resulting matrix contains outcomes in monotonically decreasing probabilities, and that the returned matrix is of the correct shape.

---
+++ test/scenario.py
@@ -0,0 +1,35 @@
+import unittest
+from src.scenario import get_top_scenarios
+from src.constants import services
+import pandas as pd
+import numpy as np
+
+
+class TestTopScenarioGeneration(unittest.TestCase):
+    @classmethod
+    def setUpClass(self):
+        price_df = pd.read_csv("data/sa_fcas_data.csv", parse_dates=["SETTLEMENTDATE"])
+
+        for s in services:
+            price_df[s + "PROB"] = (price_df[s + "LOCALDISPATCH"] / price_df[s + "ACTUALAVAILABILITY"]).clip(upper=1)
+
+        price_df["PERIODID"] = price_df["SETTLEMENTDATE"].apply(lambda x : str(x.hour).zfill(2) + str(x.minute).zfill(2))
+
+        prob_df = price_df.groupby("PERIODID")[[s + "PROB" for s in services]].mean().reset_index()
+
+        self.scenario_length = 14
+        self.num_scenarios = 2**self.scenario_length
+        self.prob_vec = prob_df[services[0] + 'PROB'][:self.scenario_length].values
+        self.top_scenarios = get_top_scenarios(self.num_scenarios, self.prob_vec)
+
+    def test_is_monotonic_decreasing(self):
+        scenario_probs = np.prod(self.top_scenarios * self.prob_vec, axis=1)
+
+        self.assertTrue(np.all(np.diff(scenario_probs) <= 0))
+
+    def test_is_correct_shape(self):
+        self.assertTrue(self.top_scenarios.shape == (self.num_scenarios, self.scenario_length))
+
+
+if __name__ == "__main__":
+    unittest.main()
