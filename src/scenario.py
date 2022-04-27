2dcdd867d8a83c257994ddb261a378756cfe542f
Francis Dela Cruz <50617340+fdc981@users.noreply.github.com>
2022-04-27 17:03:24 +0930

    Change to log probability, fix flip_scenario
    This commit makes two changes. First, instead of sorting scenarios in terms of probabilities, the log of the probabilities is used instead. This prevents precision issues that may arise with larger scenario lengths due to multiplying many fractional numbers (e.g. such as length 288 scenarios). Second, flip_scenario() has been changed to fix an issue on indexing: the integer scenarios have to be indexed MSB first instead of LSB first as in the previous revision. This ensures that the indexed bits correspond to the correct scenario probability.

--- src/scenario.py
+++ src/scenario.py
@@ -33,7 +33,7 @@ def scenario_to_int(bitstr):
     return sum((2**i * bit for i, bit in enumerate(bitstr[::-1])))
 
 
-def int_scenario_probability(index, success_probs):
+def int_scenario_log_probability(index, success_probs):
     """Output probability of scenario given by index.
 
     Args:
@@ -49,10 +49,10 @@ def int_scenario_probability(index, success_probs):
     scenario_length = len(success_probs)
     scenario = int_to_scenario(index, scenario_length)
 
-    return np.prod(scenario * success_probs + (1 - scenario) * (1 - success_probs))
+    return np.sum(np.log(scenario * success_probs + (1 - scenario) * (1 - success_probs)))
 
 
-def flip_scenario(int_scenario, prob, index, success_prob):
+def flip_scenario(int_scenario, scenario_prob, index, success_prob, scenario_length):
     """Flip the `index` bit of scenario `bitstr`, returning a pair
     containing the new scenario and its associated probability.
 
@@ -63,23 +63,25 @@ def flip_scenario(int_scenario, prob, index, success_prob):
         success_prob: the probability of success
 
     Returns:
-        a pair `(prob, scenario)` with `scenario` being the new
-        scenario in bitstring form and `prob` being the associated probability.
+        a pair `(log_prob, scenario)` with `scenario` being the new
+        scenario in bitstring form and `log_prob` being the associated
+        natural log of the probability.
     """
-    bit = (int_scenario & 2**index) == 2**index
+    flip_pos = scenario_length - index - 1
+    bit = (int_scenario & 2**flip_pos) == 2**flip_pos
 
-    prob /= (bit * success_prob + (1 - bit) * (1 - success_prob))
+    scenario_prob -= np.log(bit * success_prob + (1 - bit) * (1 - success_prob))
 
     bit = not bit
 
-    prob *= (bit * success_prob + (1 - bit) * (1 - success_prob))
+    scenario_prob += np.log(bit * success_prob + (1 - bit) * (1 - success_prob))
 
     if bit == 0:
-        new_scenario = int_scenario - (2**index)
+        new_scenario = int_scenario - (2**flip_pos)
     else:
-        new_scenario = int_scenario + (2**index)
+        new_scenario = int_scenario + (2**flip_pos)
 
-    return (prob, new_scenario)
+    return (scenario_prob, new_scenario)
 
 
 def possible_next_scenarios(bitstr, scenario_prob, success_probs):
@@ -96,7 +98,9 @@ def possible_next_scenarios(bitstr, scenario_prob, success_probs):
     """
     scenario_length = len(success_probs)
     for flip_pos in range(scenario_length):
-        new_prob, new_scenario = flip_scenario(bitstr, scenario_prob, flip_pos, success_probs[flip_pos])
+        new_prob, new_scenario = flip_scenario(bitstr, scenario_prob, flip_pos,
+                                               success_probs[flip_pos],
+                                               success_probs.size)
         yield (new_prob, new_scenario)
 
 
@@ -114,7 +118,7 @@ def get_top_scenarios(n, success_probs):
     """
     top_scenario = scenario_to_int([0 if p < 0.5 else 1 for p in success_probs])
 
-    q = [(-int_scenario_probability(top_scenario, success_probs), top_scenario)]
+    q = [(-int_scenario_log_probability(top_scenario, success_probs), top_scenario)]
 
     top_scenarios = []
     encountered = set([e[1] for e in q])
@@ -125,6 +129,7 @@ def get_top_scenarios(n, success_probs):
     while i < iter_limit and len(q) > 0:
         i += 1
         top_prob, top_scenario = heapq.heappop(q)
+        top_prob = -top_prob
         top_scenarios.append(top_scenario)
         for prob, scenario in possible_next_scenarios(top_scenario, top_prob, success_probs):
             elem = (-prob, scenario)
